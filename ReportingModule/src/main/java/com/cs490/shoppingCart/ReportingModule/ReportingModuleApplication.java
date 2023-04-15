199519ff0a889268c3aa51ebc38ef826e7114be4
nq-masuku <72672255+nq-masuku@users.noreply.github.com>
2023-04-15 17:26:50 -0500

    reporting tool integration updated now

--- ReportingModule/src/main/java/com/cs490/shoppingCart/ReportingModule/ReportingModuleApplication.java
+++ ReportingModule/src/main/java/com/cs490/shoppingCart/ReportingModule/ReportingModuleApplication.java
@@ -1,13 +1,36 @@
 package com.cs490.shoppingCart.ReportingModule;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class ReportingModuleApplication {
+public class ReportingModuleApplication implements CommandLineRunner {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ReportingModuleApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportingModuleApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		LOG.info("###############################################################################################");
+		LOG.info("          Reporting Module Spring Boot Application command-line initiated");
+		LOG.info("###############################################################################################");
+	}
+
+	@RestController
+	class RESTController {
+
+		@GetMapping("/")
+		public String hello() {
+			return "<H1>Reporting Module Spring Boot Application Activated</H1>";
+		}
+	}
 }
--- ReportingModule/src/main/java/com/cs490/shoppingCart/ReportingModule/web/ReportingModuleController.java
+++ ReportingModule/src/main/java/com/cs490/shoppingCart/ReportingModule/web/ReportingModuleController.java
@@ -1,46 +1,108 @@
 package com.cs490.shoppingCart.ReportingModule.web;
 
-import java.util.List;
-
+import com.cs490.shoppingCart.ReportingModule.service.ReportingService;
+import com.cs490.shoppingCart.ReportingModule.service.SalesDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 /**
  * The Class ReportingController.
  */
-@CrossOrigin
 @RestController
+@CrossOrigin
 @RequestMapping("/reporting")
-public class ReportingModuleController{
+public class ReportingModuleController {
+
+    @Autowired
+    private ReportingService reportingService;
 
 
+    @GetMapping("/sales")
+    public ResponseEntity<?> getSales(
+            @RequestParam(value="days", required = false) String days,
+            @RequestParam(value="productName", required = false) String productName,
+            @RequestParam(value="category", required = false) String category,
+            @RequestParam(value="vendorId", required = false) String vendorId) {
 
-    /**
-     * Generate report endpoint.
-     *
-     * @param reportDTO the report object
-     * @return the String
-     */
-    @PostMapping(path = "/report")
-    @ResponseBody
-    public ResponseEntity<String> generateReport(@RequestBody ReportDTO report){
+        CustomErrorType error=new CustomErrorType("No sales found for the given input.");
+        if(days != null){
+            Optional<SalesDTO> sales = reportingService.getSalesByDays(days);
+            return responseEntityForOptional(sales,error);
+        }
+        if(productName != null){
+            Optional<SalesDTO> sales = reportingService.getSalesByProduct(productName);
+            return responseEntityForOptional(sales,error);
+        }
+        if(category != null){
+            Optional<SalesDTO> sales = reportingService.getSalesByCategory(category);
+            return responseEntityForOptional(sales,error);
+        }
+        if(vendorId != null){
+            Optional<SalesDTO> sales = reportingService.getSalesByVendor(vendorId);
+            return responseEntityForOptional(sales,error);
+        }
+        return null;
 
-        return new ResponseEntity<>("Reports will be generated soon", HttpStatus.OK);
+    }
+
+    @GetMapping("/annual-profit")
+    public ResponseEntity<?> getAnnualProfit(
+            @RequestParam(value="vendorId", required = false) String vendorId) {
+
+        CustomErrorType error=new CustomErrorType("No records found for profit calculations");
+        if(vendorId != null){
+            Optional<SalesDTO> sales = reportingService.getAnnulProfitByVendor(vendorId);
+            return responseEntityForOptional(sales,error);
+        }
+        else{
+            Optional<SalesDTO> sales = reportingService.getTotalAnnualProfit();
+            return responseEntityForOptional(sales,error);
+        }
+
+    }
+
+    @GetMapping("/annual-loss")
+    public ResponseEntity<?> getAnnualLoss(
+            @RequestParam(value="vendorId", required = false) String vendorId) {
+
+        CustomErrorType error=new CustomErrorType("No records found for loss calculations");
+        if(vendorId != null){
+            Optional<SalesDTO> sales = reportingService.getAnnulLossByVendor(vendorId);
+            return responseEntityForOptional(sales,error);
+        }
+        else{
+            Optional<SalesDTO> sales = reportingService.getTotalAnnualLoss();
+            return responseEntityForOptional(sales,error);
+        }
+
+    }
+
+    @GetMapping("/annual-revenue")
+    public ResponseEntity<?> getAnnualRevenue(
+            @RequestParam(value="vendorId", required = false) String vendorId) {
+
+        CustomErrorType error=new CustomErrorType("No records found for revenue calculations");
+        if(vendorId != null){
+            Optional<SalesDTO> sales = reportingService.getAnnulRevenueByVendor(vendorId);
+            return responseEntityForOptional(sales,error);
+        }
+        else{
+            Optional<SalesDTO> sales = reportingService.getTotalAnnualRevenue();
+            return responseEntityForOptional(sales,error);
+        }
+
     }
 
 
-    @GetMapping("/")
-    public String hello() {
-        return "<H1>Reporting Module Spring Boot Application Activated</H1>";
+    public ResponseEntity<?> responseEntityForOptional(Optional<SalesDTO> sales, CustomErrorType error){
+        if(sales.isPresent()){
+            return new ResponseEntity<>(sales.get(), HttpStatus.OK);
+        } return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
 
 }
