6b9c03ca683124b0fcd58b7b97da09b26cd91a1c
nq-masuku <72672255+nq-masuku@users.noreply.github.com>
2023-04-18 19:22:33 -0500

    reporting mod updated

--- ReportingModule/src/main/java/com/cs490/shoppingCart/ReportingModule/service/AddressDTO.java
+++ ReportingModule/src/main/java/com/cs490/shoppingCart/ReportingModule/service/AddressDTO.java
@@ -1,5 +1,12 @@
 package com.cs490.shoppingCart.ReportingModule.service;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressDTO {
     private String address_id;
     private String street;
@@ -7,61 +14,4 @@ public class AddressDTO {
     private String state;
     private String zipcode;
     private String country;
-
-    public AddressDTO(String address_id, String street, String city, String state, String zipcode, String country) {
-        this.address_id = address_id;
-        this.street = street;
-        this.city = city;
-        this.state = state;
-        this.zipcode = zipcode;
-        this.country = country;
-    }
-
-    public String getAddress_id() {
-        return address_id;
-    }
-
-    public void setAddress_id(String address_id) {
-        this.address_id = address_id;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
 }
