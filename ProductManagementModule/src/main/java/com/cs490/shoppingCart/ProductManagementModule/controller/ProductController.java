0512585de6fa8afa32d92a1bd681b0b9ff0b9619
sophearyrin1 <srin@miu.edu>
2023-04-20 10:19:44 -0500

    unit test product managment module

--- ProductManagementModule/src/main/java/com/cs490/shoppingCart/ProductManagementModule/controller/ProductController.java
+++ ProductManagementModule/src/main/java/com/cs490/shoppingCart/ProductManagementModule/controller/ProductController.java
@@ -149,28 +149,28 @@ public class ProductController {
      * This is optional code
      * Upload image when create a product
      */
-    @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
-        return new ResponseEntity<>(productService.uploadFile(file), HttpStatus.OK);
-    }
-
-
-    @GetMapping("/download/{fileName}")
-    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
-        byte[] data = productService.downloadFile(fileName);
-        ByteArrayResource resource = new ByteArrayResource(data);
-        return ResponseEntity
-                .ok()
-                .contentLength(data.length)
-                .header("Content-type", "application/octet-stream")
-                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
-                .body(resource);
-    }
-
-    @DeleteMapping("/delete/{fileName}")
-    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
-        return new ResponseEntity<>(productService.deleteFile(fileName), HttpStatus.OK);
-    }
+//    @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+//    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+//        return new ResponseEntity<>(productService.uploadFile(file), HttpStatus.OK);
+//    }
+//
+//
+//    @GetMapping("/download/{fileName}")
+//    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+//        byte[] data = productService.downloadFile(fileName);
+//        ByteArrayResource resource = new ByteArrayResource(data);
+//        return ResponseEntity
+//                .ok()
+//                .contentLength(data.length)
+//                .header("Content-type", "application/octet-stream")
+//                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+//                .body(resource);
+//    }
+//
+//    @DeleteMapping("/delete/{fileName}")
+//    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+//        return new ResponseEntity<>(productService.deleteFile(fileName), HttpStatus.OK);
+//    }
 
     @GetMapping("/productDetail")
     public List<ListProductResponseSpecificID> getAllProductWithSpecificIDList(@RequestParam(required = true) Set<Long> productId){
