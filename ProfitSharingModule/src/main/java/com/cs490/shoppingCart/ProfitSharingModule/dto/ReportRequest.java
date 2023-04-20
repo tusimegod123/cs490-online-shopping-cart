a91055f0fb152734c82d6cfc7e010bfc936eaa96
selamawityilma <selamawit.woldeyohannes@miu.edu>
2023-04-20 11:09:01 -0500

    add-endpoint-for-profit-and-revenue

--- ProfitSharingModule/src/main/java/com/cs490/shoppingCart/ProfitSharingModule/dto/ReportRequest.java
+++ ProfitSharingModule/src/main/java/com/cs490/shoppingCart/ProfitSharingModule/dto/ReportRequest.java
@@ -3,20 +3,15 @@ package com.cs490.shoppingCart.ProfitSharingModule.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 import java.util.Date;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProfitRequest {
-
-    private Long transactionId;
-
-    private Long orderId;
-
-    private Double transactionValue;
-
-    private String transactionNumber;
-
-    private Date transactionDate;
+@Data
+public class ReportRequest {
+    private LocalDate fromDate;
+    private LocalDate toDate;
+    private Long userId;
 }
--- ProfitSharingModule/src/main/java/com/cs490/shoppingCart/ProfitSharingModule/repository/ProfitRepository.java
+++ ProfitSharingModule/src/main/java/com/cs490/shoppingCart/ProfitSharingModule/repository/ProfitRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface ProfitRepository extends JpaRepository<Profit, Long> {
+    List<Profit> findAllByTransactionDateIsBetween(Date fromDate, Date toDate);
+    List<Profit> findAllByUserIdAndTransactionDateIsBetween(Long id, Date fromDate, Date toDate);
 
 }
