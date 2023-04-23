41d5cb2ab615776bb7b71a49f68421f18f05ccca
sophearyrin1 <srin@miu.edu>
2023-04-23 18:34:38 -0500

    fixed

--- ProductManagementModule/src/main/java/com/cs490/shoppingCart/ProductManagementModule/model/Product.java
+++ ProductManagementModule/src/main/java/com/cs490/shoppingCart/ProductManagementModule/model/Product.java
@@ -1,9 +1,8 @@
 package com.cs490.shoppingCart.ProductManagementModule.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
@@ -21,7 +20,11 @@ public class Product {
     private Double price;
     private Integer qty;
     private Double itemCost;
+
+    @Column(columnDefinition="TEXT")
     private String description;
+
+    @Column(columnDefinition="TEXT")
     private String imageUrl;
     private Boolean verified;
 
